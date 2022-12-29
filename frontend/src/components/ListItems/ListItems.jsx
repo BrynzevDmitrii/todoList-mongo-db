196fea874295243aec747737dddfd67629989cb1
BrynzevDmitrii <Elf_happy@mail.ru>
2022-12-29 20:37:15 +0300

    dinamic is chekced

--- frontend/src/components/ListItems/ListItems.jsx
+++ frontend/src/components/ListItems/ListItems.jsx
@@ -1,37 +1,63 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import clsx from "clsx";
-import styles from "./ListItems.module.scss";
+import axios from "axios";
 import { Checkbox } from "@mui/material";
+import styles from "./ListItems.module.scss";
 
 export const ListItems = (props) => {
+  const[isFinished, setIsFinished]=useState();
 
-  const idRef = useRef();
 
-const hendelChekced=(e)=>{
-console.log(idRef);
-}
+  useEffect(()=>{},[])
+   
 
+  const idRef = useRef(null);
+
+  const hendelChekced = async (e) => {
+   
+    const myHeaders = new Headers();
+    const id = idRef.current.id
+    const itemId = e.target.id
+
+    const list = await (await axios.get(`http://localhost:3001/lists/${id} `)).data
+
+    const snapShot = structuredClone(list)
+    setIsFinished(snapShot.items[itemId].isChecked)
+    const modSnap = snapShot.items[itemId].isChecked=!isFinished
+  
+   
+    await axios.put(`http://localhost:3001/lists/${id} `,
+    snapShot,
+    myHeaders.set('Content-Type','application/json; charset=utf-8'),
+    )
+};
 
   return (
-<>
-            {props.title}
-            <ul key={props.id + props.title} className={styles.df}>
-        {props.item.map((i,indx) => {
+    <>
+      {props.title}
+      <ul
+        key={props.id + props.title}
+        className={styles.df}
+        id={props.id}
+        ref={idRef}
+      >
+        {props.item.map((i, indx) => {
           return (
-              <li key={props.id+indx}
-                ref ={idRef}
-                className={clsx(
-                  styles.item,
-                  i.isChecked && styles.underline
-                )}
-              >
-                <Checkbox checked = {i.isChecked} onClick = {(e)=>hendelChekced(e)}/>
-                &nbsp; {i.item}
-              </li>  
+            <li
+              id={indx}
+              key={indx}
+              className={clsx(styles.item, i.isChecked && styles.underline)}
+              onClick={(e) => hendelChekced(e)}
+             
+            >
+              <Checkbox checked={i.isChecked}
+              
+                />
+               {i.item}
+            </li>
           );
         })}
-          </ul>
-    
+      </ul>
     </>
   );
 };
