501a41e0f40dbd765407f910ef6e639bb5d4c0a6
BrynzevDmitrii <Elf_happy@mail.ru>
2023-01-05 13:57:51 +0300

    add Skeleton

--- frontend/src/components/ListItems/ListItems.jsx
+++ frontend/src/components/ListItems/ListItems.jsx
@@ -4,10 +4,11 @@ import axios from "axios";
 import { Items } from "../Items/Item";
 
 import styles from "./ListItems.module.scss";
+import { PostSkeleton } from "../RemvUpgrd/Skeleton";
 
 export const ListItems = () => {
   const [allLists, setAllLists] = useState([]);
-  const [reload, setReload] = useState()
+  const [reload, setReload] = useState(0)
 
   useEffect(() => {
     axios("http://localhost:3001/lists").then((res) => setAllLists(res.data));
@@ -19,14 +20,25 @@ export const ListItems = () => {
 
   return (
     <>
-      {allLists.map((i) => {
+      {allLists.length ? 
+      allLists.map((i) => {
         return (
           <ul key={i.indx + i.title} className={styles.df} id={i.id}>
-            {i.title}
+            <h3 className={styles.title}>{i.title}</h3>
             <Items items={i.items} isCheck={isCheck} />
           </ul>
         );
-      })}
+      })
+      :
+      <>
+      <PostSkeleton />
+      <PostSkeleton />
+      <PostSkeleton />
+      <PostSkeleton />
+      </>
+      
+      
+      }
     </>
   );
 };
