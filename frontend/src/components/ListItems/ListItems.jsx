023c6b73765936eca196bc9838b591b0f2349525
BrynzevDmitrii <Elf_happy@mail.ru>
2023-01-13 19:44:58 +0300

    Edit&Remuve button

--- frontend/src/components/ListItems/ListItems.jsx
+++ frontend/src/components/ListItems/ListItems.jsx
@@ -5,11 +5,13 @@ import { Items } from "../Items/Item";
 
 import styles from "./ListItems.module.scss";
 import { PostSkeleton } from "../RemvUpgrd/Skeleton";
+import { RemvUpgrd } from "../RemvUpgrd";
 import allList from "../../store/allList";
 
 
 export const ListItems = observer(() => {
   const [reload, setReload] = useState(0);
+  const [hover, setHover ] = useState(false)
 
 
   useEffect(() => {
@@ -20,14 +22,26 @@ export const ListItems = observer(() => {
     setReload(reloaded)
   }
 
+  const hendlerOver=()=>{
+    setHover(true)
+  }
+
+  const handlerOut=()=>{
+    setHover(false)
+  }
+
   return (
     <>
       {allList.data.length ? 
-      allList.data.map((i) => {
+      allList.data.map((i,indx) => {
         return (
-          <ul key={i.indx + i.title} className={styles.df} id={i.id}>
+          <ul key={i.indx + i.title} className={styles.df} id={i.id} onMouseOver={()=>hendlerOver()}
+          onMouseOut={()=>handlerOut()}>
             <h3 className={styles.title}>{i.title}</h3>
             <Items items={i.items} isCheck={isCheck} />
+            <div className={styles.edidSection}>
+            <RemvUpgrd isEditable = {true} key={indx}/>
+            </div>
           </ul>
         );
       })
