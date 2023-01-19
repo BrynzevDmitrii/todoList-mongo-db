6f4c5da41f662b1c15f45f4d80b0a27ca978f55d
BrynzevDmitrii <Elf_happy@mail.ru>
2023-01-19 18:36:45 +0300

    CastomAlert

--- frontend/src/components/castomAlert/CastomAlert.jsx
+++ frontend/src/components/castomAlert/CastomAlert.jsx
@@ -2,15 +2,18 @@ import { Box, Button, Modal, Typography } from "@mui/material"
 
 import styles from "./CastomAlert.module.scss"
 
-export const CastomAlert = ({open})=>{
+export const CastomAlert = ({open, onClosed, handleDelited })=>{
  return(
-    <Modal open={open} className={styles["moduleWindow"]}>
-        <>
-        <Typography align="center" variant="h5">Вы точно хотите удалить весь список?</Typography>
-            <Button variant="outlined" color="error">Да, конечно!</Button>
-            <Button variant="contained" color="success">О нет что вы!!!!</Button>
-        </>
+    
+    <Modal open={open} hideBackdrop={true} disableEscapeKeyDown={true}>
+        <Box className={styles.moduleWindow}>
+        <Typography padding="10px" variant="h5">Вы точно хотите удалить весь список?</Typography>
+        <div className={styles.buttonBox} >
+            <Button variant="contained" color="success" onClick={()=>onClosed()}>О нет, что вы !!!!</Button>
+            <Button variant="outlined" color="error" onClick={()=>handleDelited()}>Да, конечно !</Button>
+        </div>   
+        </Box>
     </Modal>
-
+   
  )
 }
